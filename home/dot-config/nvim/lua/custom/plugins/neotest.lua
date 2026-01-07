22afee8c31e80c28b5754866cd5a18409550a04b
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2026-01-07 15:55:23 +0000

    Drop old nvim plugin, fix conflicting shortcuts

--- home/dot-config/nvim/lua/custom/plugins/neotest.lua
+++ home/dot-config/nvim/lua/custom/plugins/neotest.lua
@@ -3,7 +3,6 @@ return {
   dependencies = {
     'nvim-neotest/nvim-nio',
     'nvim-lua/plenary.nvim',
-    'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-python',
     'nvim-neotest/neotest-go',
@@ -11,10 +10,10 @@ return {
   keys = {
     { '<leader>tn', function() require('neotest').run.run() end, desc = 'Run nearest test' },
     { '<leader>tf', function() require('neotest').run.run(vim.fn.expand '%') end, desc = 'Run file tests' },
-    { '<leader>ts', function() require('neotest').summary.toggle() end, desc = 'Toggle test summary' },
+    { '<leader>tS', function() require('neotest').summary.toggle() end, desc = 'Toggle test summary' },
     { '<leader>to', function() require('neotest').output.open { enter = true } end, desc = 'Show test output' },
     { '<leader>tO', function() require('neotest').output_panel.toggle() end, desc = 'Toggle output panel' },
-    { '<leader>tS', function() require('neotest').run.stop() end, desc = 'Stop test' },
+    { '<leader>tx', function() require('neotest').run.stop() end, desc = 'Stop test' },
     { '[T', function() require('neotest').jump.prev { status = 'failed' } end, desc = 'Prev failed test' },
     { ']T', function() require('neotest').jump.next { status = 'failed' } end, desc = 'Next failed test' },
   },
--- home/dot-config/nvim/lua/custom/plugins/trouble.lua
+++ home/dot-config/nvim/lua/custom/plugins/trouble.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   cmd = 'Trouble',
   keys = {
-    { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
+    { '<leader>xd', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
     { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
     { '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>', desc = 'Symbols (Trouble)' },
     { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
