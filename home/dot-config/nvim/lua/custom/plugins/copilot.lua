5dd0abda8ec591641f7b10587d9d225ee823b5c1
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2026-01-23 09:52:39 +0000

    Swap to blink cmp as a trial

--- home/dot-config/nvim/lua/custom/plugins/copilot.lua
+++ home/dot-config/nvim/lua/custom/plugins/copilot.lua
@@ -1,20 +1,27 @@
 -- Copilot - lazy loaded on demand via :CopilotEnable
 -- Uses ~900MB per instance, so only enable when you really need it
+-- With blink.cmp, we use copilot's native inline suggestion mode
 return {
   {
     'zbirenbaum/copilot.lua',
     cmd = 'CopilotEnable', -- only load when this command is run
     config = function()
       require('copilot').setup {
-        suggestion = { enabled = false },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<M-l>', -- Alt+l to accept suggestion
+            accept_word = '<M-w>', -- Alt+w to accept word
+            accept_line = '<M-j>', -- Alt+j to accept line
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<M-e>',
+          },
+        },
         panel = { enabled = false },
       }
-      require('copilot_cmp').setup()
-      print 'Copilot enabled'
+      print 'Copilot enabled (use Alt+l to accept suggestions)'
     end,
-  },
-  {
-    'zbirenbaum/copilot-cmp',
-    lazy = true, -- loaded by copilot.lua config above
   },
 }
--- home/dot-config/nvim/lua/kickstart/plugins/autopairs.lua
+++ home/dot-config/nvim/lua/kickstart/plugins/autopairs.lua
@@ -1,16 +1,10 @@
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
+-- Note: blink.cmp has built-in auto_brackets for function completions,
+-- so we don't need the cmp integration here anymore.
 
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
-    require('nvim-autopairs').setup {}
-    -- If you want to automatically add `(` after selecting a function or method
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  end,
+  opts = {},
 }
