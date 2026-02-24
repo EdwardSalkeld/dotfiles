b44e6528c7603916e077d454428c6842381e8eee
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2026-02-24 11:26:27 +0000

    markdown toggles

--- home/dot-config/nvim/lua/custom/plugins/markdown.lua
+++ home/dot-config/nvim/lua/custom/plugins/markdown.lua
@@ -10,6 +10,30 @@ return {
     },
   },
   {
+    'nfrid/markdown-togglecheck',
+    dependencies = { 'nfrid/treesitter-utils' },
+    ft = { 'markdown' },
+    opts = {},
+    keys = {
+      {
+        '<leader>mt',
+        function()
+          require('markdown-togglecheck').toggle()
+        end,
+        desc = '[M]arkdown [T]oggle checkbox',
+        ft = 'markdown',
+      },
+      {
+        '<leader>mn',
+        function()
+          require('markdown-togglecheck').toggle_box()
+        end,
+        desc = '[M]arkdown [N]ew checkbox toggle',
+        ft = 'markdown',
+      },
+    },
+  },
+  {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     ---@module 'render-markdown'
